a075276ac4d9730586afdc5926cb6f35e17ceafe
Evan Vang <evankvang@gmail.com>
2026-08-25 12:35:17 -0500

    Add project files.

---
+++ FileReadingBeginnings/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace FileReadingBeginnings
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
